38ee97dedc508b493efd2d96a992e3980fe91517
Plinio Mabesi <pliniomabesi@gmail.com>
2024-01-09 18:36:56 -0300

    Update footer

--- src/components/dashboard/Footer/Footer.tsx
+++ src/components/dashboard/Footer/Footer.tsx
@@ -4,29 +4,37 @@ import { Container } from 'react-bootstrap'
 export default function Footer() {
   return (
     <footer className="footer border-top px-sm-2 py-2">
+      
       <Container fluid className="text-center align-items-center flex-column flex-md-row d-flex justify-content-between">
-        <div>
-          <a className="text-decoration-none" href="https://coreui.io">CoreUI Admin Template</a>
-          {' '}
-          © 2023
-          creativeLabs.
-        </div>
         
+        <div className="text-center align-items-center flex-column flex-md-row d-flex justify-content-between" >
+
+          <div>
+            <a className="text-decoration-none" href="https://coreui.io">CoreUI Admin Template</a>
+            {' '}
+            © 2023 creativeLabs.
+          </div>
+
+          <div>&nbsp;&nbsp;</div>
+
+          <div className="ms-md-auto">
+            Powered by&nbsp;
+            <a
+              className="text-decoration-none"
+              href="@app/ui/dashboard/AdminLayout"
+            >
+              CoreUI UI
+              Components
+            </a>
+          </div>
+
+        </div>
+
         <div className="ms-md-auto">
           Implementation Example by&nbsp;
           <a className="text-decoration-none" href="https://github.com/mabesi">Mabesi</a>
         </div>
 
-        <div className="ms-md-auto">
-          Powered by&nbsp;
-          <a
-            className="text-decoration-none"
-            href="@app/ui/dashboard/AdminLayout"
-          >
-            CoreUI UI
-            Components
-          </a>
-        </div>
       </Container>
     </footer>
   )
